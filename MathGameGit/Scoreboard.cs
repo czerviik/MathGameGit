2115e59d34b846234f7bd81ec86de071263ada6c
Jiří Malík <czerviik@gmail.com>
2023-09-21 12:08:25 +0200

    add the Scoreboard class

---
+++ MathGameGit/Scoreboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace MathGameGit
+{
+	public class Scoreboard
+	{
+		public int Score { get; private set; }
+        public int Rounds { get; private set; }
+
+        public Scoreboard()
+		{
+			Score = 0;
+			Rounds = 1;
+		}
+
+		public void ScoreUp()
+		{
+			Score++;
+		}
+
+		public void RoundUp()
+		{
+			Rounds++;
+		}
+
+		public void Display()
+		{
+			Console.WriteLine("Round {0}, score{1}",Rounds,Score);
+		}
+	}
+}
+
